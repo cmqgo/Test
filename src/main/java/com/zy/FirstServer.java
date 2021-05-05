59e7c1cb2ae028bdc91d5157be47a730307da82d
cmqgo <c13711475313@163.com>
2021-05-05 22:33:16 +0800

    初始化

---
+++ src/main/java/com/zy/FirstServer.java
@@ -0,0 +1,13 @@
+package com.zy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class FirstServer {
+	public static void main(String[] args){
+		SpringApplication.run(FirstServer.class, args);
+	}
+}
